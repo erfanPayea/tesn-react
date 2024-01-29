e0e3adc2c255b5d2180b971c1e1dc9d08fa9b5eb
ruzbeh <ruzbeh.moini@gmail.com>
2024-01-29 17:24:33 +0330

    request to back for sign up added

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,19 +1,91 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 import './SignUp.css'; // Import CSS for styling
 
 const SignUp = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmation, setConfirmation] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [error, setError] = useState('');
+    // const handleUsernameChange = (event) => {
+    //     setUsername(event.target.value);
+    // };
+    //
+    // const handlePasswordChange = (event) => {
+    //     setPassword(event.target.value);
+    // };
+    //
+    // const handleEmailChange = (event) => {
+    //     setEmail(event.target.value);
+    // };
+
+    useEffect(() => {
+        if (password === confirmation) {
+            setError("")
+        } else {
+            setError("password and password confirmation did not match")
+        }
+    }, [password, confirmation]);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        if(confirmation !== password) {
+            setError("password and password confirmation did not match");
+            return;
+        }
+        // Perform login logic here with username and password
+        // For example, you can send an API request or handle authentication logic
+        console.log('Login submitted:', username, password, phone);
+
+        // Prepare the data object
+        const data = {
+            username: username,
+            phone: phone,
+            password: password
+        };
+
+        try {
+            // Send the POST request to the server
+            const response = await fetch('http://127.0.0.1:8000/user/', {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            const result = await response.json();
+            const token = result["token"]
+            localStorage.setItem("token", token)
+            alert("Success!")
+        } catch (error) {
+            // Handle any error that occurred during the request
+            console.error('Error:', error);
+        }
+
+    };
+
+
   return (
     <div className="signup-container">
       <h2>Sign Up</h2>
-      <form>
-        <input type="text" placeholder="Username" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <input type="password" placeholder="Confirm Password" />
-        <button type="submit">Sign Up</button>
-        <p>Already have an account? <Link to="/" className="signin-link">Sign In</Link></p>
-      </form>
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={username} placeholder="username" required={true}
+                   onChange={(e) => setUsername(e.target.value)}/>
+            <input type="email" value={email} placeholder="email" required={true}
+                   onChange={(e) => setEmail(e.target.value)}/>
+            <input type="password" value={password} placeholder="Password" required={true}
+                   onChange={(e) => setPassword(e.target.value)}/>
+            <input type="password" value={confirmation} placeholder="Confirm Password" required={true}
+                   onChange={(e) => setConfirmation(e.target.value)}/>
+            <input type="phone" value={phone} placeholder="phone"
+                   onChange={(e) => setPhone(e.target.value)}/>
+            <button type="submit">Sign Up</button>
+            <p>Already have an account? <Link to="/" className="signin-link">Sign In</Link></p>
+            <p>{error}</p>
+        </form>
     </div>
   );
 };
