d29a45a44045289fc6c9e241254d56d82b68f9d8
ruzbeh <ruzbeh.moini@gmail.com>
2024-01-30 21:11:30 +0330

    request to back for profile added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom'; // Import Link from React Router
 import './Home.css'; // Import CSS for styling
 
 const Home = () => {
-  return (
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        // Perform login logic here with username and password
+        // For example, you can send an API request or handle authentication logic
+        console.log('Login submitted:', username, password);
+
+        // Prepare the data object
+        const data = {
+            username: username,
+            password: password
+        };
+
+        try {
+            // Send the POST request to the server
+            const response = await fetch('http://127.0.0.1:8000/user/token', {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            const result = await response.json();
+            const token = result["token"]
+            localStorage.setItem("token", token)
+            alert("Success!")
+        } catch (error) {
+            // Handle any error that occurred during the request
+            // console.error('Error:', error);
+            setError("username and password did not matched");
+        }
+    };
+
+    return (
     <div className="home-container">
       <div className="welcome-message">
         <h1>Welcome to Travel Social Network!</h1>
@@ -11,13 +47,16 @@ const Home = () => {
       </div>
       <div className="auth-form">
         <h2>Sign In or Sign Up</h2>
-        <form>
-          <input type="text" placeholder="Username" />
-          <input type="password" placeholder="Password" />
-          <button type="submit">Sign In</button>
-          {/* Change the anchor tag to a Link component */}
-          <p>Don't have an account? <Link to="/signup">Sign Up</Link></p>
-        </form>
+          <form onSubmit={handleSubmit}>
+              <input type="text" value={username} placeholder="username" required={true}
+                     onChange={(e) => setUsername(e.target.value)}/>
+              <input type="password" value={password} placeholder="Password" required={true}
+                     onChange={(e) => setPassword(e.target.value)}/>
+              <button type="submit">Sign In</button>
+              {/* Change the anchor tag to a Link component */}
+              <p>Don't have an account? <Link to="/signup">Sign Up</Link></p>
+              <p>{error}</p>
+          </form>
       </div>
     </div>
   );
--- src/pages/UserProfilePage.js
+++ src/pages/UserProfilePage.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './UserProfilePage.css'; // Import CSS for styling
 import UserHighlight from '../components/User/UserHighlights'; // Corrected import path
 import UserPost from '../components/User/UserPosts'; // Corrected import path
 import UserReview from '../components/User/UserReviews'; // Corrected import path
 
 const UserProfilePage = () => {
+    // const [error, setError] = useState('');
+    const [data, setData] = useState({})
+    const getData = async () => {
+        // Perform login logic here with username and password
+        // For example, you can send an API request or handle authentication logic
+        try {
+            // Send the POST request to the server
+            const response = await fetch('http://127.0.0.1:8000/user/', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'token ' + localStorage.getItem('token'),
+                }
+            });
+
+            const result = await response.json();
+            setData(result)
+            // alert("Success!")
+        } catch (error) {
+            // Handle any error that occurred during the request
+            // console.error('Error:', error);
+            alert("some problems happen");
+        }
+    };
+
+    useEffect(() => {
+        getData()
+    },[]);
+
   return (
     <div className="user-profile-container">
       <div className="profile-header">
@@ -14,16 +43,18 @@ const UserProfilePage = () => {
         <div className="profile-details">
           <div className="profile-picture">
             {/* Display user's profile picture */}
-            <img src="user-profile-picture.jpg" alt="Profile" />
+            {/*<img src="user-profile-picture.jpg" alt="Profile" />*/}
           </div>
-          <div className="user-info">
-            <h1>John Doe</h1> {/* Display user's username */}
-            <p>@johndoe</p> {/* Display additional information */}
-          </div>
+            <div className="user-info">
+                <h1> {"username:" + data.username}</h1> {/* Display user's username */}
+                <h3> {"joined at:" + data.date_joined}</h3>
+                <p>{"id : " + data.id}</p> {/* Display additional information */}
+                <p>{"membership : " + data.membership}</p>
+            </div>
         </div>
       </div>
 
-      {/* User highlights section */}
+        {/* User highlights section */}
       <div className="user-section">
         <h2>Highlights</h2>
         <div className="user-highlights">
